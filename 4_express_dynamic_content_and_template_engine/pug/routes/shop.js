b8b547de027bf695d5cd4f7884e11f89eb97f782
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-12-20 01:40:08 +0500

    - converted app from html pages to Pug template engine

---
+++ 4_express_dynamic_content_and_template_engine/pug/routes/shop.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  const { products } = adminData;
+  res.render("shop", { products, docTitle: "My Shop", path: "/" });
+});
+
+module.exports = router;
