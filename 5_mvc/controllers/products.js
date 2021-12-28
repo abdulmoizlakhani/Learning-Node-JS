04598da475cec5ab49e03a4c3fdfd02a3b41c307
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-12-28 17:44:31 +0500

    - created model for product

--- 5_mvc/controllers/products.js
+++ 5_mvc/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/Product");
 
 exports.getAddProduct = (req, res) => {
   res.render("add-product", {
@@ -8,13 +8,14 @@ exports.getAddProduct = (req, res) => {
 };
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
   res.render("shop", {
-    products,
+    products: Product.fetchAll(),
     docTitle: "My Shop",
     path: "/",
   });
